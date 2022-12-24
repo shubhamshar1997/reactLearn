5b5d8cf300f298f3fbaae8294743d6f083b7655f
shubhamsharma1996 <shubhamsen216@gmail.com>
2022-12-24 15:54:02 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement('h1', { id: 'title', className: "woho" }, 'Hello From Shubham')
+const heading1 = React.createElement('h1', { id: 'title2', className: "woho2" }, 'Hello From Simran')
+const container = React.createElement('div', { id: 'container' }, [heading, heading1])
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(container)
